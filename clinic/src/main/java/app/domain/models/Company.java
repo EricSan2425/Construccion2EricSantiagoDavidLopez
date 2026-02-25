11b425d8650b25c4bbd559759f74f77f4c05905b
EricSan2425 <eric.david@correo.tdea.edu.co>
2026-02-25 17:06:52 -0500

    Add files via upload

---
+++ clinic/src/main/java/app/domain/models/Company.java
@@ -0,0 +1,15 @@
+package app.domain.models;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+
+public class Company {
+    private Long id;
+    private String name;
+    
+}
